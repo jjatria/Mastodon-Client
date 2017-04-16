649e6a502e7c89db110b771bc60f556b469274dd
José Joaquín Atria <jjatria@gmail.com>
2017-04-16 20:32:45 +0100

    Update example script to use coerced entities

--- t/example/mastodon.pl
+++ t/example/mastodon.pl
@@ -9,16 +9,19 @@ use diagnostics;
 use Mastodon::Client;
 use Log::Any qw( $log );
 use Log::Any::Adapter;
-Log::Any::Adapter->set( 'Stdout' );
+Log::Any::Adapter->set( 'Stdout', log_level => 'info' );
 
 use Config::Tiny;
 my $config = (scalar @ARGV) ? Config::Tiny->read( $ARGV[0] )->{_} : {};
-my $app = Mastodon::Client->new( $config );
+my $app = Mastodon::Client->new({
+  %{$config},
+  coerce_entities => 1,
+});
 
 my $listener = $app->stream( name => 'public' );
 
 $listener->on( update => sub {
-  my ($listener, $msg) = @_;
+  my ($listener, $status) = @_;
 
   use Term::ANSIColor qw(:constants);
   use HTML::FormatText::WithLinks;
@@ -27,10 +30,10 @@ $listener->on( update => sub {
   local $Term::ANSIColor::AUTORESET = 1;
 
   print BOLD BLUE sprintf("%s (%s):\n",
-    $msg->{account}{display_name},
-    $msg->{account}{acct},
+    $status->account->display_name,
+    $status->account->acct,
   );
-  print $f->parse($msg->{content});
+  print $f->parse($status->content);
 });
 $listener->start;
 
@@ -38,11 +41,11 @@ $listener->start;
 # # Defaults to 'user'
 # my $listener = $app->stream( name => 'public' );
 # $listener->on( update => sub {
-#   my ($listener, $msg) = @_;
+#   my ($listener, $status) = @_;
 #   $log->infof('%s (%s) says: %s',
-#     $msg->{account}{display_name},
-#     $msg->{account}{acct},
-#     $msg->{content}
+#     $status->account->display_name,
+#     $status->account->acct,
+#     $status->content
 #   );
 # });
 # $listener->start;

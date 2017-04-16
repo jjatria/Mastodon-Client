0d29bea17ea00167db7e7e24e886a0bbc8a3bfca
José Joaquín Atria <jjatria@gmail.com>
2017-04-16 23:04:51 +0100

    Clarify usage of streaming example

--- t/examples/public_stream.pl
+++ t/examples/public_stream.pl
@@ -7,11 +7,17 @@ use strict;
 use diagnostics;
 
 use Mastodon::Client;
-use Log::Any qw( $log );
-use Log::Any::Adapter;
-Log::Any::Adapter->set( 'Stdout', log_level => 'info' );
-
 use Config::Tiny;
+
+unless (scalar @ARGV) {
+  print "  Missing arguments
+  USAGE: $0 <CONFIG>
+
+  <CONFIG> should be an INI file with a valid 'client_secret', 'client_id', and
+  'access_token', and an 'instance' key with the URL to a Mastodon instance.\n";
+  exit(1);
+}
+
 my $config = (scalar @ARGV) ? Config::Tiny->read( $ARGV[0] )->{_} : {};
 my $app = Mastodon::Client->new({
   %{$config},
@@ -36,42 +42,3 @@ $listener->on( update => sub {
   print $f->parse($status->content);
 });
 $listener->start;
-
-# # Subscribe to public stream
-# # Defaults to 'user'
-# my $listener = $app->stream( name => 'public' );
-# $listener->on( update => sub {
-#   my ($listener, $status) = @_;
-#   $log->infof('%s (%s) says: %s',
-#     $status->account->display_name,
-#     $status->account->acct,
-#     $status->content
-#   );
-# });
-# $listener->start;
-#
-# # Get timeline as hash reference
-# # Defaults to "home"
-# $app->timeline;
-# $app->timeline( name => 'home' );
-# $app->timeline( tag => 'perl', local => 1 );
-# $app->timeline( tag => 'perl' );
-# $app->timeline( name => 'public', local => 1 );
-# $app->timeline( name => 'public' );
-#
-# # Register app in instance and obtain client_id and client_secret
-# # $app->register;
-#
-# # Get the authorization URL for this app
-# $app->authorization_url;
-# $app->authorization_url(
-#   instance => 'mastodon.cloud',
-# );
-#
-# # Authorize an application
-# $app->authorize( access_code => 'asdasdasd' );
-# $app->authorize(
-#   username => 'my@email.com',
-#   password => $pass,
-# );
-#
